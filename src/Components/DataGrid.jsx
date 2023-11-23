4d94576f1024c38bd84e16ec9d5d0523c1d27673
DevPeter <polalekan526@gmail.com>
2023-11-23 23:45:49 +0100

    Data fetched

--- src/Components/DataGrid.jsx
+++ src/Components/DataGrid.jsx
@@ -1,17 +1,34 @@
 import useFetch from "../data/usefetch";
+import { useState } from "react";
+import Pagination from "./Pagination";
 
 const DataGrid = () => {
     const {data, loading } = useFetch('https://api.spacexdata.com/v3/capsules')
-    console.log(data[1].status)
+    // console.log(data[1].status)
 
-    const paginatedData = data && data.data ? Object.values(data.data).slice(startIndex, endIndex) : [];
+   const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10; 
 
-  const totalPages = data && data.data ? Math.ceil(Object.values(data.data).length / itemsPerPage) : 0;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const paginatedData = data && data ? Object.values(data).slice(startIndex, endIndex) : [];
+  console.log(paginatedData)
+
+  const totalPages = data && data ? Math.ceil(Object.values(data).length / itemsPerPage) : 0;
   const currentSerialNumber = (currentPage - 1) * itemsPerPage + 1
     return ( 
         <>
                 <section>
-                
+{paginatedData.map((item, index)=>(
+                <div>{item.status}</div>
+)
+
+)}
+
+                {totalPages > 1 && (
+                     <Pagination totalPages={totalPages} onPageChange={setCurrentPage} />
+                             )}
                 </section>
         </>
      );
